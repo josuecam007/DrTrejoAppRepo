a50be56e8fba9d43ade1547f30efcdd6621a11e1
josuecam007 <josue.cam007@gmail.com>
2020-04-22 20:19:44 -0500

    servicios full

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../assets/styles/components/Servicios.scss';
-import fracturaLuxacion from '../assets/static/servicios/brazo-fractura.png';
-import ortopedicos from '../assets/static/servicios/espina.png';
+import fracturaLuxacion from '../assets/static/servicios/fractura_de_antebrazo_v2.png';
+import articulaciones from '../assets/static/servicios/articulaciones.png';
 import pieTobillo from '../assets/static/servicios/pie.png';
 import mano from '../assets/static/servicios/muneca.png';
 import brazo from '../assets/static/servicios/brazo-antebrazo.png';
 import hombro from '../assets/static/servicios/hombro.png';
-//import articulares from '../assets/static/servicios/pie.png';
+import espina from '../assets/static/servicios/espina.png';
 import rodilla from '../assets/static/servicios/rodilla.png';
+import infeccion from '../assets/static/servicios/infeccion-muscoesqueletico.png';
+import jugador from '../assets/static/servicios/deporte.png';
+import pierna from '../assets/static/servicios/muslo-pierna.png';
+import ortopedicos from '../assets/static/servicios/ortopedicos.png';
 
 const Servicios = () => {
   return (
     <div className='servicios_section'>
       <h1>Servicios</h1>
+      <h1>Padecimientos del paciente adulto y pediatrico:</h1>
       <ul className='servicios-container'>
         <li>
           <img src={fracturaLuxacion} alt='imagen_fractura' className='servicios__img' />
@@ -35,11 +40,11 @@ const Servicios = () => {
           Lesiones de brazo, antebrazo y codo
         </li>
         <li>
-          <img src={ortopedicos} alt='imagen_fractura' className='servicios__img' />
+          <img src={pierna} alt='imagen_fractura' className='servicios__img' />
           Muslo y pierna
         </li>
         <li>
-          <img src={ortopedicos} alt='imagen_fractura' className='servicios__img' />
+          <img src={articulaciones} alt='imagen_fractura' className='servicios__img' />
           Lesiones articulares
         </li>
         <li>
@@ -51,15 +56,15 @@ const Servicios = () => {
           Lesiones de hombro
         </li>
         <li>
-          <img src={ortopedicos} alt='imagen_fractura' className='servicios__img' />
+          <img src={jugador} alt='imagen_fractura' className='servicios__img' />
           lesiones deportivas y prescripcion del ejercicio
         </li>
         <li>
-          <img src={ortopedicos} alt='imagen_fractura' className='servicios__img' />
+          <img src={espina} alt='imagen_fractura' className='servicios__img' />
           Lesiones de la columna incluyendo dolor de origen ciatico
         </li>
         <li>
-          <img src={ortopedicos} alt='imagen_fractura' className='servicios__img' />
+          <img src={infeccion} alt='imagen_fractura' className='servicios__img' />
           Infecciones y tumores del sistema musculoesqueletico
         </li>
       </ul>
